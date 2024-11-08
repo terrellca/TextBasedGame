eb60b4de10d33308231121bcbfb94f63559a0a79
Cedric <cjterrell04@gmail.com>
2024-11-07 20:19:08 -0500

    Added Title/Start/ChoiceButtons/Labels

--- src/package1/Game.java
+++ src/package1/Game.java
@@ -1,7 +1,13 @@
 package package1;
 
+import java.awt.event.ActionEvent;
+
+import java.awt.event.ActionListener;
+
 public class Game
 {
+
+    ChoiceHandler cHandler = new ChoiceHandler();
 
     UI ui = new UI();
 
@@ -12,7 +18,36 @@ public class Game
 
         public Game()
         {
-            ui.createUI();
+            ui.createUI(cHandler);
+        }
+        public class ChoiceHandler implements ActionListener
+        {
+            public void actionPerformed(ActionEvent event )
+            {
+                String yourChoice = event.getActionCommand();
+
+                switch(yourChoice)
+                {
+                    case "start":
+                    break;
+                    case "c1":
+                    break;
+                    
+                    case "c2":
+                    break;
+
+                    case "c3":
+                    break;
+
+                    case "c4":
+                    break;
+
+                    default:
+                    break;
+
+                  
+                }
+            }
         }
 
 
