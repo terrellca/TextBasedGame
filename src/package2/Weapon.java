f365897a499e47ad44d57a0b56479ca69f0aee89
Cedric <cjterrell04@gmail.com>
2024-11-18 14:59:00 -0500

    Created ArrayList to hold weapons to cut down on multiple classes being made and also created WeaponFactory

--- src/package2/Weapon.java
+++ src/package2/Weapon.java
@@ -4,5 +4,39 @@ public class Weapon
 {
     public String name;
     public int damage;    
+    public double weight;
+
+    public Weapon(String name, int damage, double weight)
+    {
+        this.name = name;
+        this.damage = damage;
+        this.weight = weight;
+    }
+
+
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getDmg()
+    {
+        return damage;
+    }
+
+    public double getWeight()
+    {
+        return weight;
+    }
+
+    /*
+     * public String toString()
+    {
+        return name + 
+    }
+     * 
+     */
     
+
 }
